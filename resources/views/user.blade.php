77244aa5cb04207a990b8cfe0ba889a03818eda6
ajisyahroni <aji.0508@students.amikom.ac.id>
2020-03-25 21:14:14 +0700

    ADD: procfile and edit user blade

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,11 +1,97 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
-<body>
+
+<body class="container">
     
+    <!-- <form>
+        <label for="harga">Harga</label>
+        <br>
+        <select class="form-control" name="harga" id="harga">
+            <option value="5">Sangat Penting</option>
+            <option value="4">Penting</option>
+            <option value="3">Cukup Penting</option>
+            <option value="2">Tidak Penting</option>
+            <option value="1">Sangat Tidak Penting</option>
+        </select>
+
+        <br>
+        <label for="luas_kamar">Luas Kamar</label>
+        <br>
+        <select class="form-control" name="luas_kamar" id="luas_kamar">
+            <option value="5">Sangat Penting</option>
+            <option value="4">Penting</option>
+            <option value="3">Cukup Penting</option>
+            <option value="2">Tidak Penting</option>
+            <option value="1">Sangat Tidak Penting</option>
+        </select>
+
+
+        <br>
+        <label for="jarak">Jarak</label>
+        <br>
+        <select class="form-control" name="jarak" id="jarak">
+            <option value="5">Sangat Penting</option>
+            <option value="4">Penting</option>
+            <option value="3">Cukup Penting</option>
+            <option value="2">Tidak Penting</option>
+            <option value="1">Sangat Tidak Penting</option>
+        </select>
+
+
+        <br>
+        <label for="fasilitas_kamar">Fasilitas Kamar</label>
+        <br>
+        <select class="form-control" name="fasilitas_kamar" id="fasilitas_kamar">
+            <option value="5">Sangat Penting</option>
+            <option value="4">Penting</option>
+            <option value="3">Cukup Penting</option>
+            <option value="2">Tidak Penting</option>
+            <option value="1">Sangat Tidak Penting</option>
+        </select>
+
+
+        <br>
+        <label for="fasilitas_penunjang">Fasilitas Penunjang</label>
+        <br>
+        <select class="form-control" name="fasilitas_penunjang" id="fasilitas_penunjang">
+            <option value="5">Sangat Penting</option>
+            <option value="4">Penting</option>
+            <option value="3">Cukup Penting</option>
+            <option value="2">Tidak Penting</option>
+            <option value="1">Sangat Tidak Penting</option>
+        </select>
+
+
+        <br>
+        <label for="fasilitas_lingkungan">Fasilitas linkungan</label>
+        <br>
+        <select class="form-control" name="fasilitas_lingkungan" id="fasilitas_lingkungan">
+            <option value="5">Sangat Penting</option>
+            <option value="4">Penting</option>
+            <option value="3">Cukup Penting</option>
+            <option value="2">Tidak Penting</option>
+            <option value="1">Sangat Tidak Penting</option>
+        </select>
+
+        <br>
+
+        <label for="tipe">Tipe</label>
+        <br />
+        <select class="form-control" name="tipe" id="tipe">
+            <option value="pria">pria</option>
+            <option value="wanita">wanita</option>
+            <option value="campur">campur</option>
+        </select>
+        <br>
+        <input class="btn btn-block btn-info" type="submit" value="submit">
+    </form> -->
 </body>
+
 </html>
